5fdc362085a7c0ad3178ea5918f30d85006608a4
Vladislav <tekuzar@mail,ru>
2025-01-13 21:16:24 +0300

    Done!

--- src/test/java/com/example/Calculator/service/CalculatorServiceTest.java
+++ src/test/java/com/example/Calculator/service/CalculatorServiceTest.java
@@ -1,4 +1,5 @@
 package com.example.Calculator.service;
+import com.example.Calculator.eception.DevideByZeroExeprion;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,19 @@ public class CalculatorServiceTest {
         Integer num2 = 3;
         Integer expectedResult = 5;
 
+        Integer num3 = 15;
+        Integer num4 = 20;
+        Integer expectedResult2 =  35;
+
         //when
         Integer actualResult = calculatorService.plus(num1, num2);
+        Integer actualResult2 = calculatorService.plus(num3,num4);
 
         //Then
         Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedResult2, actualResult2);
     }
+
 
     @Test
     public void minus() {
@@ -28,11 +36,17 @@ public class CalculatorServiceTest {
         Integer num2 = 2;
         Integer expectedResult = 4;
 
+        Integer num3 = 40;
+        Integer num4 = 25;
+        Integer expectedResult2 =  15;
+
         //when
         Integer actualResult = calculatorService.minus(num1, num2);
+        Integer actualResult2 = calculatorService.minus(num3, num4);
 
         //Then
         Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedResult2, actualResult2);
     }
 
     @Test
@@ -42,12 +56,21 @@ public class CalculatorServiceTest {
         Integer num2 = 2;
         Integer expectedResult = 6;
 
+        //given
+        Integer num3 = 2;
+        Integer num4 = 1;
+        Integer expectedResult2 = 2;
+
         //when
         Integer actualResult = calculatorService.multiply(num1, num2);
+        Integer actualResult2 = calculatorService.multiply(num3, num4);
 
         //Then
         Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedResult2, actualResult2);
     }
+
+
 
     @Test
     public void devide() {
@@ -63,4 +86,17 @@ public class CalculatorServiceTest {
         Assertions.assertEquals(expectedResult, actualResult);
 
     }
+
+    @Test
+    public void shouldThrowExeptionIfNum2IsZero () {
+        //given
+        Integer num1 = 4;
+        Integer num2 = 0;
+
+        //when
+        //Then
+        Assertions.assertThrows(DevideByZeroExeprion.class, () -> calculatorService.devide(num1, num2));
+
+    }
+
 }
